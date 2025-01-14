92fde43757799a9e7d0de37e268b8aaf3ac809bc
shopia-sal <shopiasalma599@gmail.com>
2025-01-14 05:23:51 -0800

    ubah

--- settings/documents.ts
+++ settings/documents.ts
@@ -34,7 +34,7 @@ export const Documents: Paths[] = [
     items: [
       {
         title: "Analisis Kebutuhan",
-        href: "/analisis_kebutuhan",
+        href: "",
         items : [
           {
             title: "Business Requirement",
