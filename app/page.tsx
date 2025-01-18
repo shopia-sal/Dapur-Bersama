745210e67c7a77b93d1cd09bfc46a8386d5d24a9
shopia-sal <shopiasalma599@gmail.com>
2025-01-17 21:58:26 -0800

    ubah

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
-        Kitchen Whuz
+        Kitchen Whiz
       </h1>
       <div className="flex items-center gap-5">
         <Link
